9744018250c1f340fcd3531d1a57f600046a23f9
niteshkumar <niteshkumar@niteshkumars-MacBook-Pro.local>
2025-12-30 04:46:13 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import Food from "./components/Food"
 import Card from "./components/card-components/Card"
 import Navbar from "./components/Navbar"
 import Button from "./components/buttons/Button"
+import Student from "./components/Student";
+import UserGreeting from "./components/UserGreeting";
 
 function App() {
   return (
     <>
-      <Header />
-      <Navbar />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <hr/>
-      <Food />
-      <Button />
-      <Footer />
+      {/*<Header />*/}
+      {/*<Navbar />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<hr/>*/}
+      {/*<Food />*/}
+      {/*<Button />*/}
+      {/*<Student name={"Nitesh"} age={30} isStudent={true} />*/}
+      {/*<Student name={"Patrick"} age={15} isStudent={true} />*/}
+      {/*<Student/>*/}
+      {/*<Footer />*/}
+      <UserGreeting isLoggedIn={false} username={"BroCode"}/>
     </>
   )
 }
